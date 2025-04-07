1982cbb5fc974f9ce42d3d88b972e0ef1240c1ae
MODERN EMO <elzodxodjaniazov@gmail.com>
2025-04-07 17:02:30 +0500

    update

--- controllers/control.js
+++ controllers/control.js
@@ -9,16 +9,13 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Путь к файлу db.json
 const dbPath = path.join(__dirname, "../db.json");
 
-// Чтение данных из db.json
 function readDB() {
     const data = fs.readFileSync(dbPath);
     return JSON.parse(data);
 }
 
-// Запись данных в db.json
 function writeDB(data) {
     fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
 }
@@ -40,25 +37,21 @@ const logout = (req, res) => {
     res.redirect("login");
 };
 
-// Функция для входа
 async function login(req, res) {
     try {
         const { email, password } = req.body;
-        const db = readDB(); // Читаем данные из db.json
+        const db = readDB(); 
 
-        // Поиск пользователя по email
         const user = db.users.find(u => u.email === email);
         if (!user) {
             return res.status(400).json({ success: false, message: "Неверный email или пароль" });
         }
 
-        // Проверка пароля
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(400).json({ success: false, message: "Неверный email или пароль" });
         }
 
-        // Создание токена
         const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: "1h" });
         console.log(token)
         res.cookie('accessToken', token, {maxAge: 1000*60*60*24*7, httponly: true})
@@ -69,30 +62,26 @@ async function login(req, res) {
     }
 }
 
-// Функция для регистрации
 async function register(req, res) {
     try {
         const { email, password } = req.body;
-        const db = readDB(); // Читаем данные из db.json
+        const db = readDB(); 
 
-        // Проверка на существующего пользователя
         const existingUser = db.users.find(u => u.email === email);
         if (existingUser) {
             return res.status(400).json({ success: false, message: "Email уже используется" });
         }
 
-        // Хеширование пароля
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        // Создание нового пользователя
         const newUser = {
-            id: db.users.length + 1, // Генерация ID
+            id: db.users.length + 1,
             email,
             password: hashedPassword
         };
 
-        db.users.push(newUser); // Добавление пользователя в массив
-        writeDB(db); // Запись обновлённых данных в db.json
+        db.users.push(newUser); 
+        writeDB(db); 
 
         res.redirect('/login');
     } catch (error) {
